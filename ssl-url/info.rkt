6f6cd468d4db728afc1f8decf1346501c258c6ba
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-18 15:52:10 -0700

    Add some fields to mollify "raco planet create ssl-url/"

--- ssl-url/info.rkt
+++ ssl-url/info.rkt
@@ -1,3 +1,6 @@
 #lang setup/infotab
 
 (define scribblings '(("manual.scrbl" ())))
+(define name "ssl-url")
+(define blurb '(p "Functions mirroring those in net/url, but which work with SSL."))
+(define primary-file "ssl-url.rkt")
