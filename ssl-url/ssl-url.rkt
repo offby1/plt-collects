7eacfd2ff288631828c7a54cb86a8dd1626b040b
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-18 14:41:02 -0700

    Add some whitespace

--- ssl-url/ssl-url.rkt
+++ ssl-url/ssl-url.rkt
@@ -8,13 +8,17 @@
          net/url-sig net/url-unit
          net/tcp-sig net/tcp-unit
          net/ssl-tcp-unit)
+
 (define-values/invoke-unit
   (compound-unit/infer (import) (export url^) (link tcp@ url@))
   (import) (export url^))
+
 (define ssl-tcp@ (make-ssl-tcp@ #f #f #f #f #f #f #f))
+
 (define-values/invoke-unit
   (compound-unit (import) (export URL)
     (link [((TCP : tcp^)) ssl-tcp@]
           [((URL : url^)) url@ TCP]))
   (import) (export (prefix ssl: url^)))
+
 (provide (all-defined-out))
